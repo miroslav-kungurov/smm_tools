19a7508f5c61f76ff184d4ea4ce5ddf1bf72c761
Miroslav Kungurov <56649199+miroslav-kungurov@users.noreply.github.com>
2024-09-24 12:05:21 +0300

    Update tg_reactions_count.py

--- tg_reactions_count.py
+++ tg_reactions_count.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import openpyxl
 from pyrogram import Client
 
@@ -8,27 +9,34 @@ api_hash = ''
 workbook = openpyxl.Workbook()
 sheet = workbook.active
 
-# Заполняем первый столбец названием канала и второй столбец для ссылки на пост
+# Заполняем заголовки столбцов
 sheet['A1'] = 'Канал'
 sheet['B1'] = 'Ссылка на пост'
-sheet['C1'] = 'Сумма реакций'
+sheet['C1'] = 'Количество просмотров'
+sheet['D1'] = 'Сумма реакций'
 
 # Счетчик строк и столбцов
 row = 2
-column = 4  # Столбцы с эмодзи начинаются с четвертого столбца
+column = 5  # Столбцы с эмодзи начинаются с пятого столбца
 
 # Словарь для хранения всех возможных реакций
 all_reactions = {}
 
 first_post_id = 1
-last_post_id = 2884
-channel = "@itmemlib"
+last_post_id = 9248
+channel = "@progbook"
 
 with Client("my_account", api_id, api_hash) as client:
     for message_id in range(first_post_id, last_post_id):
         print('ID поста: ', message_id)
         try:
             message = client.get_messages(chat_id=channel, message_ids=message_id)
+
+            # Проверяем количество просмотров
+            views = message.views if message.views is not None else 0
+            if views == 0:
+                print(f"Пропускаем пост {message_id} (нет просмотров)")
+                continue  # Пропускаем этот пост и переходим к следующему
 
             # Заполняем первый столбец названием канала
             sheet.cell(row=row, column=1, value=channel)
@@ -38,6 +46,9 @@ with Client("my_account", api_id, api_hash) as client:
             sheet.cell(row=row, column=2).hyperlink = post_link
             sheet.cell(row=row, column=2).value = post_link
             sheet.cell(row=row, column=2).style = "Hyperlink"
+
+            # Заполняем третий столбец количеством просмотров
+            sheet.cell(row=row, column=3, value=views)
 
             # Проверяем, есть ли реакции на сообщение
             total_reactions = 0
@@ -52,8 +63,8 @@ with Client("my_account", api_id, api_hash) as client:
                         column += 1
                     sheet.cell(row=row, column=all_reactions[emoji], value=count)
 
-            # Записываем сумму реакций в последний столбец
-            sheet.cell(row=row, column=3, value=total_reactions)
+            # Записываем сумму реакций в четвертый столбец
+            sheet.cell(row=row, column=4, value=total_reactions)
 
             row += 1
         except Exception as e:
@@ -61,9 +72,10 @@ with Client("my_account", api_id, api_hash) as client:
 
 # Заполняем нулями отсутствующие реакции
 for r in range(2, row):
-    for c in range(4, column):
+    for c in range(5, column):
         if sheet.cell(row=r, column=c).value is None:
             sheet.cell(row=r, column=c, value=0)
 
 # Сохраняем книгу Excel
-workbook.save("telegram_reactions.xlsx")
+xlsx_name = channel[1:] + "_telegram_reactions.xlsx"
+workbook.save(xlsx_name)
